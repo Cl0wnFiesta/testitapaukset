e52c75dec407c642cae950c2d36f7f0e64833220
Cl0wnFiesta <clownfiesta98@gmail.com>
2023-11-25 17:02:44 +0200

    s

--- src/main/java/com/example/TimeUtils.java
+++ src/main/java/com/example/TimeUtils.java
@@ -2,40 +2,45 @@ package com.example;
 
 public class TimeUtils {
 
-    public static int timeToSec(String time) {
-        String[] part = time.split(":");
-        int hh = Integer.parseInt(part[0]);
-        int mm = Integer.parseInt(part[1]);
-        int ss = Integer.parseInt(part[2]);
+  public static int timeToSec(String time) {
+    String[] part = time.split(":");
+    int hh = Integer.parseInt(part[0]);
+    int mm = Integer.parseInt(part[1]);
+    int ss = Integer.parseInt(part[2]);
 
-        return 3600 * hh + 60 * mm + ss;
+    return 3600 * hh + 60 * mm + ss;
+  }
+
+  public static String secToTime(int a) {
+    int hh, mm, ss;
+
+    // If a contains a string, then return "-1"
+
+    if (Double.isNaN(a)) {
+      return "-1";
     }
 
-    public static String secToTime(int a) {
-        int hh, mm, ss;
-
-        if (a < 0 || a >= 86400) {
-            return "-1";
-        }
-
-        hh = a / 3600;
-        a = a - (3600 * hh);
-        mm = a / 60;
-        ss = a - (60 * mm);
-
-        String res = hh + ":";
-        if (mm <= 10) {
-            res = res + "0" + mm + ":";
-        } else {
-            res = res + mm + ":";
-        }
-        if (ss <= 10) {
-            res = res + "0" + ss;
-        } else {
-            res = res + ss;
-        }
-
-        return res;
+    if (a < 0 || a >= 86400) {
+      return "-1";
     }
+
+    hh = a / 3600;
+    a = a - (3600 * hh);
+    mm = a / 60;
+    ss = a - (60 * mm);
+
+    String res = hh + ":";
+    if (mm <= 10) {
+      res = res + "0" + mm + ":";
+    } else {
+      res = res + mm + ":";
+    }
+    if (ss <= 10) {
+      res = res + "0" + ss;
+    } else {
+      res = res + ss;
+    }
+
+    return res;
+  }
 }
-
